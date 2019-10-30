c64464eb36132c95ca6a96f11267ca8e82a1e83b
ehe781 <emanuel.hegedus18@gmail.com>
2019-10-30 15:26:01 +0100

    cambio

--- Sistemas Operativos/Aventura 2/prueba1.c
+++ Sistemas Operativos/Aventura 2/prueba1.c
@@ -1,2 +1,12 @@
 #include    <stdio.h>
 
+int main() {
+    int COMMAND_LINE_SIZE;
+    COMMAND_LINE_SIZE = 6;
+    char line[COMMAND_LINE_SIZE];
+    while(scanf(line)){
+        if(line=="exit" && line == "^D"){
+            salir del shell
+        }
+    }
+}
